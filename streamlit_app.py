704798b76950c4f4e88b61613956c57223d2f464
Igbokweuche Chukwuebuka Emmanuel <65443492+bukky300@users.noreply.github.com>
2026-01-13 13:40:16 +0100

    Fix URL formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # List of Streamlit App URLs
 
 STREAMLIT_APPS = [
+    "https://bukky-research-tool.streamlit.app/",
     "https://bukky-vehicle-damage-prediction.streamlit.app/",
     "https://bukky-ml-project-credit-risk-model.streamlit.app/",
-    "https://bukky-ml-project-health-insurance-prediction.streamlit.app/",
-    "https://bukky-research-tool.streamlit.app/"
+    "https://bukky-ml-project-health-insurance-prediction.streamlit.app/"
 ]
